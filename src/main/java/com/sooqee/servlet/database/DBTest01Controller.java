be78edf4226609bb53799a6388b86a10b73794ae
DESKTOP-EFOL5DI\ssp <0414soyeon@gmail.com>
2022-08-01 00:28:01 +0900

    [김소연] -jsp (database) test01 내림차순, insert, 개수제한 출력

--- src/main/java/com/sooqee/servlet/database/DBTest01Controller.java
+++ src/main/java/com/sooqee/servlet/database/DBTest01Controller.java
@@ -1,5 +1,7 @@
 package com.sooqee.servlet.database;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -13,23 +15,62 @@ import com.sooqee.servlet.common.MysqlService;
 @WebServlet("/db/test01")
 public class DBTest01Controller extends HttpServlet {
 	
-	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		
 		response.setContentType("text/plain");
+		
+		PrintWriter out = response.getWriter();
 		
 		MysqlService mysqlService = MysqlService.getInstance();
 		
 		mysqlService.connect();
 		
-		String selectQuery = "SELECT * FROM `real_estate`;";
+		// 내림차순
+		String selectQuery = "SELECT * FROM `real_estate` ORDER BY `id` DESC;";
 		
 		ResultSet resultSet = mysqlService.select(selectQuery);
 		
+		
+		// insert
+//		String insertQuery = "INSERT INTO `real_estate`\r\n"
+//				+ "(`realtorId`, `address`, `area`, `type`, `price`, `rentPrice`, `createdAt`, `updatedAt`)\r\n"
+//				+ "VALUE\r\n"
+//				+ "(3, '헤라필리스 101동 5305호', 350, '매매', 500000, NULL, now(), now());";
+//		
+		//int count = mysqlService.update(insertQuery);
+
+		
+		// 전제 출력
+//		try {
+//			while(resultSet.next()) {
+//				String address = resultSet.getString("address");
+//				int area = resultSet.getInt("area");
+//				String type = resultSet.getString("type");
+//				
+//				
+//				
+//				out.println("매물 주소 : " + address + ", 면적 : " + area + ", 타입 : " + type);
+//			}
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+	
+		
+		// 10 개만 출력
+		int sum = 0;
 		try {
 			while(resultSet.next()) {
-				String realtorId = resultSet.getString("realtorId");
 				String address = resultSet.getString("address");
+				int area = resultSet.getInt("area");
+				String type = resultSet.getString("type");
 				
+				sum ++;
+				out.println("매물 주소 : " + address + ", 면적 : " + area + ", 타입 : " + type);
+				if(sum == 10) {
+					break;
+				}
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
